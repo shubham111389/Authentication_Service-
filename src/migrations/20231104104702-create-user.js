299bd91276027f0b5ce4b6f3b03200bd3f4aac31
shubham annpurne <shubhamannpurne1029@gmail.com>
2023-11-04 17:41:32 +0530

    prepared user model

--- src/migrations/20231104104702-create-user.js
+++ src/migrations/20231104104702-create-user.js
@@ -10,10 +10,19 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 300]
+        }
       },
       createdAt: {
         allowNull: false,
--- src/models/user.js
+++ src/models/user.js
@@ -14,8 +14,21 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [3, 100]
+      }
+    }
   }, {
     sequelize,
     modelName: 'User',
