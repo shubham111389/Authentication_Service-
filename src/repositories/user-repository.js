f48b9195a08e71faea22dbd99211e8ad888eb83e
shubham annpurne <shubhamannpurne1029@gmail.com>
2023-11-05 14:17:41 +0530

    Added token gen logic from sign in

--- src/repositories/user-repository.js
+++ src/repositories/user-repository.js
@@ -36,6 +36,17 @@ class UserRepository {
             throw error;
         }
     }
+    async getByEmail(userEmail) {
+        try {
+            const user = await User.findOne({where: {
+                email: userEmail
+            }});
+            return user;
+        } catch (error) {
+            console.log("Something went wrong on repository layer");
+            throw error;
+        }
+    }
 }
 
 module.exports = UserRepository;
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -45,6 +45,25 @@ class UserService {
             throw error;
         }
     }
+    async signIn(email, plainPassword) {
+        try {
+            // step 1-> fetch the user using the email
+            const user = await this.userRepository.getByEmail(email);
+            // step 2-> compare incoming plain password with stores encrypted password
+            const passwordsMatch = this.checkPassword(plainPassword, user.password);
+
+            if(!passwordsMatch) {
+                console.log("Password doesn't match");
+                throw {error: 'Incorrect password'};
+            }
+            // step 3-> if passwords match then create a token and send it to the user
+            const newJWT = this.createToken({email: user.email, id: user.id});
+            return newJWT;
+        } catch (error) {
+            console.log("Something went wrong in the sign in process");
+            throw error;
+        }
+    }
 
 
 }
